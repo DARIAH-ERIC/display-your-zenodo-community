6d0a86ebf492b84479893f84c061956f2436f120
yoannspace <yoann.moranville@gmail.com>
2020-12-18 12:30:31 +0100

    Remove useless init filter that made double queries to the API

--- includes/class-display-your-zenodo-community.php
+++ includes/class-display-your-zenodo-community.php
@@ -142,7 +142,6 @@ class Display_Your_Zenodo_Community {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
-		$this->loader->add_filter( 'init', $plugin_public, 'zp_retrieve_json' );
 		$this->loader->add_filter( 'query_vars', $plugin_public, 'zp_query_vars' );
 	}
 
